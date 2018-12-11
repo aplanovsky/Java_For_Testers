c05ba748fb1d3287c379303694562ebdcc8da694
aplanovsky <aplanovsky@gmail.com>
2018-12-11 20:00:07 +0300

    Лямда-функция вычисление максимального идентификатора

--- addressbook-web-tests/src/test/java/my/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/my/addressbook/tests/GroupCreationTests.java
@@ -4,7 +4,6 @@ package my.addressbook.tests;
 import my.addressbook.model.GroupDate;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,13 +20,7 @@ public class GroupCreationTests extends TestBase {
     Assert.assertEquals(after.size(), before.size() + 1);
 
 
-    int max = 0;
-    for(GroupDate g : after ){
-      if(g.getId() > max){
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId( after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
