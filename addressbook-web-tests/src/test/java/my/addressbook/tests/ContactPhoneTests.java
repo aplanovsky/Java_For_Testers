1264f3171a381eac479c9ac19d33a65d575c95ee
aplanovsky <aplanovsky@gmail.com>
2018-12-14 12:23:31 +0300

    Update ContactPhoneTests.java

--- addressbook-web-tests/src/test/java/my/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/my/addressbook/tests/ContactPhoneTests.java
@@ -14,6 +14,7 @@ public class ContactPhoneTests extends TestBase {
   @Test
   public void testContactPhones() {
     app.goTo().gotoHomePage();
+    ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
     assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
