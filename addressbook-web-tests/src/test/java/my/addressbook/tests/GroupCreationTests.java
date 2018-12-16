63cfa40cecdc6439c97d7eff37f48063625ab057
aplanovsky <aplanovsky@gmail.com>
2018-12-16 03:35:54 +0300

    Формат XML

--- addressbook-web-tests/src/test/java/my/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/my/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,7 @@
 package my.addressbook.tests;
 
 
+import com.thoughtworks.xstream.XStream;
 import my.addressbook.model.GroupData;
 import my.addressbook.model.Groups;
 import org.testng.annotations.DataProvider;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.nullValue;
@@ -23,19 +25,28 @@ public class GroupCreationTests extends TestBase {
   @DataProvider
   public Iterator<Object[]> validGroups() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
-     String line = reader.readLine();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
+    String line = reader.readLine();
     while (line != null){
       String[] spliit =  line.split(";");
-      list.add(new  Object[]{
-              new GroupData()
-                      .withName(spliit[0])
-                      .withHeader(spliit[1])
-                      .withFooter(spliit[2])
-              } );
+        xml += line;
+//      list.add(new  Object[]{
+//              new GroupData()
+//                      .withName(spliit[0])
+//                      .withHeader(spliit[1])
+//                      .withFooter(spliit[2])
+//              } );
       line = reader.readLine();
     }
-    return list.iterator();
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+    return groups.stream()
+            .map((g) -> new Object[] {g})
+            .collect(Collectors.toList())
+            .iterator();
+    //    return list.iterator();
   }
 
 
